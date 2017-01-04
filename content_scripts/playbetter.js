6f4ef198a0643eb39b9b044c52c1eccea6bcf6bb
polypmer <exorable.ludos@gmail.com>
2017-01-04 14:00:26 -0500

    Rearrange files

---
+++ content_scripts/playbetter.js
@@ -0,0 +1,17 @@
+document.getElementById('run').onclick = function() {
+    console.log("clicked");
+};
+
+
+//var myCodeMirror = CodeMirror(document.body);
+
+console.log("Fire aeway");
+var car = {type:"Fiat", model:"500", color:"white"};
+
+window.onload = function() {
+
+    console.log("What?");
+    //chrome.tabs.executeScript(null, {
+      //  file: "/lib/codemirror.js"
+//});
+};
--- scripts/playbetter.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('run').onclick = function() {
-    console.log("clicked");
-};
-
-
-var myCodeMirror = CodeMirror(document.body);
-var car = {type:"Fiat", model:"500", color:"white"};
-console.log("What?");
