e62d3239241bcb4e67f4a4f105a1085fba7ce139
polypmer <exorable.ludos@gmail.com>
2017-01-04 16:19:12 -0500

    save textArea on click

--- content_scripts/playbetter.js
+++ content_scripts/playbetter.js
@@ -4,16 +4,20 @@ var textEdit = document.getElementById('code');
 
 console.log(CodeMirror);
 
-var mirror = CodeMirror(wrap, {
-    value: "func main() {\n        fmt.Println()\n};",
+// var mirror = CodeMirror(wrap, {
+//     value: "func main() {\n        fmt.Println()\n};",
+//     mode:  "go",
+//     lineNumbers: true
+// });
+
+
+var mirror = CodeMirror.fromTextArea(textEdit, {
     mode:  "go",
     lineNumbers: true
 });
 
+
 document.getElementById('run').onclick = function() {
     console.log("clicked");
+    mirror.save();
 };
-
-//var myCodeMirror = CodeMirror.fromTextArea(textEdit,
-//                                           {lineNumbers: true,
-//                                            mode: "go"});
