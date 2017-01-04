762637a952b46cb0a1de022127b4573dfca6e803
polypmer <exorable.ludos@gmail.com>
2017-01-04 16:15:17 -0500

    Fix lineNumbers typo

--- content_scripts/playbetter.js
+++ content_scripts/playbetter.js
@@ -5,22 +5,14 @@ var textEdit = document.getElementById('code');
 console.log(CodeMirror);
 
 var mirror = CodeMirror(wrap, {
-    value: "func main() {\nfmt.Println()\n};",
+    value: "func main() {\n        fmt.Println()\n};",
     mode:  "go",
-    lineNumber: true
+    lineNumbers: true
 });
 
 document.getElementById('run').onclick = function() {
     console.log("clicked");
 };
-
-
-//var div = document.createElement('div');
-//div.id = 'editor';
-//div.innerHTML = 'what';
-//document.body.insertBefore(div, output);
-
-
 
 //var myCodeMirror = CodeMirror.fromTextArea(textEdit,
 //                                           {lineNumbers: true,
